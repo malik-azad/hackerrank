d5d8535ef75ba60a633342d3ce58cf3a17ee4124
Azad Ahmad Malik <113776072+malik-azad@users.noreply.github.com>
2025-02-01 23:47:33 +0530

    Create Simple_If-Else.java

---
+++ Simple_If-Else.java
@@ -0,0 +1,11 @@
+ if(N % 2 == 1) //odd numbers
+         System.out.println("Weird");
+        else
+            {
+             if(N>=2 && N<=5)
+                System.out.println("Not Weird");
+             else if(N<=20)
+                System.out.println("Weird");
+                else
+                System.out.println("Not Weird");
+        }
